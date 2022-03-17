92cfc98fede9e35d076e2e07ebbccbff3819916c
EJaureguiP <emmanuel.jauregui@gmail.com>
2022-03-17 13:21:38 -0700

    added importer
    added importer

---
+++ MySqlDataImporter/DataExporter/Program.cs
@@ -0,0 +1,64 @@
+﻿using ImportExportDataLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0. xlsx full filename
+            string fileName = args[0];
+            //1. sheet name
+            string sheetName = args[1];
+            //2. columns
+            string[] columns = args[2].Split('|');
+
+
+            //3. database name
+            string databaseName = args[3];
+
+            //4. table
+            string databaseTableName = args[4];
+
+            //5. fields
+            string[] fields = args[5].Split('|');
+
+            //6. types : int, float, double, string
+            string[] types = args[6].Split('|');
+
+
+            string server = args[7];
+
+            string dbuser = args[8];
+
+            string dbpassword = args[9];
+
+
+
+            if (columns.Length != fields.Length)
+            {
+                Console.Error.WriteLine("Numbers of columns in worksheet is not equal to numbers of fields in database");
+                return;
+            }
+
+            Export importer = new Export(server, dbuser, dbpassword, databaseName);
+
+            ImportExportDataLibrary.Response response = importer.ExportToExcel(fileName, sheetName, columns, databaseTableName, fields, types);
+
+            if (response.Success)
+            {
+                Console.WriteLine("Imported!!!!");
+            }
+            else
+            {
+                Console.Error.WriteLine(response.Message);
+            }
+
+        }
+    }
+}
